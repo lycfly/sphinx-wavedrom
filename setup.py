70f1316a3d7ef24fdfc5f59a28cc92ebfe4f841a
lycfly <601753574@qq.com>
2024-02-04 21:08:08 +0800

    patch setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ project_url = 'https://github.com/bavovanachte/sphinx-wavedrom'
 
 requires = ['Sphinx>=1.8',
             'wavedrom>=1.9.0rc1',
-            'cairosvg>=2;python_version>="3.3"',
-            'xcffib;python_version>="3.3"']
+            'cairosvg>=2;python_version>="3.3"']
 
 setup(
     name='sphinxcontrib-wavedrom',
